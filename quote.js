655d0eac58b0054d330cebbfd52c852b681499b0
ashutoshpurushottam <apurushottam@icloud.com>
2018-05-01 16:42:01 +0530

    commit project

---
+++ quote.js
@@ -0,0 +1,182 @@
+var quotes = [
+	{
+		quote: "Money is the most universal and most efficient system of mutual trust ever devised",
+        topic: 'science',
+		source: "Yuval Noah Harari",
+        tags: ['Science']
+	},
+  {
+		quote: "Shallow men believe in luck or in circumstance. Strong men believe in cause and effect",
+        topic: 'science',
+		source: "Ralph Waldo Emerson",
+        tags: ['Science']
+	},
+  {
+		quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe",
+        topic: 'science',
+		source: "Albert Einstein",
+        tags: ['Science']
+	},
+  {
+		quote: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+        topic: 'science',
+		source: "Bernard M. Baruch",
+        tags: ['Science']
+	},
+  {
+		quote: "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+        topic: 'science',
+		source: "Dr. Seuss",
+        tags: ['Science']
+	},
+	{
+		quote: "Let the beauty of what you love be what you do",
+        topic: 'inspiring',
+		source: "Rumi",
+        tags: ['Inspiring']
+	},
+  {
+		quote: "Don't cry because it's over, smile because it happened.",
+        topic: 'inspiring',
+		source: "Dr. Seuss",
+        tags: ['Inspiring']
+	},
+  {
+		quote: "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+        topic: 'inspiring',
+		source: "Marilyn Monroe",
+        tags: ['Inspiring']
+	},
+  {
+		quote: "You only live once, but if you do it right, once is enough.",
+        topic: 'inspiring',
+		source: "Mae West",
+        tags: ['Inspiring']
+	},
+  {
+		quote: "Be the change that you wish to see in the world.",
+        topic: 'inspiring',
+		source: "Mahatma Gandhi",
+        tags: ['Inspiring']
+	},
+  {
+		quote: "I have nothing to lose but something to gain",
+        topic: 'philosphy',
+		source: "Eminem",
+        tags: ['Philosphy']
+	},
+  {
+		quote: "Normality is a paved road: it’s comfortable to walk but no flowers grow",
+        topic: 'philosphy',
+		source: "Vincent Van Gogh",
+        tags: ['Philosphy']
+	},
+  {
+		quote: "The true meaning of life is to plant trees, under whose shade you do not expect to sit.",
+        topic: 'philosphy',
+		source: "Nelson Henderson",
+        tags: ['Philosphy']
+	},
+	{
+		quote: "The opposite of love is indifference, and the opposite of happiness is boredom",
+        topic: 'philosphy',
+		source: "Timothy Ferriss",
+        tags: ['Philosphy']
+	},
+  {
+		quote: "In three words I can sum up everything I've learned about life: it goes on.",
+        topic: 'philosphy',
+		source: "Robert Frost",
+        tags: ['Philosphy']
+	},
+	{
+		quote: "Pleasure in the job puts perfection in the work",
+        topic: 'general',
+		source: 'Aristotle',
+        tags: ['General']
+	},
+  {
+		quote: "The true sign of intelligence is not knowledge but imagination",
+        topic: 'general',
+		source: "Albert Einstein",
+        tags: ['General']
+	},
+  {
+		quote: "No one can make you feel inferior without your consent.",
+        topic: 'general',
+		source: "Eleanor Roosevelt",
+        tags: ['General']
+	},
+  {
+		quote: "If you tell the truth, you don't have to remember anything.",
+        topic: 'general',
+		source: "Mart Twain",
+        tags: ['General']
+	},
+  {
+		quote: "A friend is someone who knows all about you and still loves you.",
+        topic: 'general',
+		source: "Elbert Hubbard",
+        tags: ['General']
+	},
+	{
+		quote: "The best way to predict the future is to create it",
+        topic: 'business',
+		source: "Peter Drucker",
+        tags: ['Business']
+	},
+	{
+		quote: "Money and success don't change people; they merely amplify what is already there",
+        topic: 'business',
+		source: "Will Smith",
+        tags: ['Business']
+	},
+	{
+		quote: "If you think you can do a thing or think you can't do a thing, you're right",
+        topic: 'business',
+		source: "Henry Ford",
+        tags: ['Business']
+	},
+  {
+		quote: "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+        topic: 'business',
+		source: "Maya Angelou",
+        tags: ['Business']
+	},
+  {
+		quote: "Always forgive your enemies; nothing annoys them so much.",
+        topic: 'business',
+		source: "Oscar Wilde",
+        tags: ['Business']
+	},
+  {
+		quote: "If today were the last day of your life, would you want to do what you are about to do today?",
+        topic: 'inspiring',
+		source: "Steve Jobs",
+        tags: ['Inspiring']
+	},
+  {
+		quote: "If today were the last day of your life, would you want to do what you are about to do today?",
+        topic: 'inspiring',
+		source: "Steve Jobs",
+        tags: ['Inspiring']
+	},
+  {
+		quote: "I love you the more in that I believe you had liked me for my own sake and for nothing else.",
+        topic: 'inspiring',
+		source: "John Keats",
+        tags: ['Inspiring']
+	},
+  {
+		quote: "But man is not made for defeat. A man can be destroyed but not defeated.",
+        topic: 'inspiring',
+		source: "Ernest Hemingway",
+        tags: ['Inspiring']
+	},
+	{
+		quote: "There is no charm equal to tenderness of heart.",
+        topic: 'inspiring',
+		source: "Jane Austen",
+        tags: ['Inspiring']
+	}
+];
---
+++ script.js
@@ -0,0 +1,116 @@
+// UI Handles
+const quotesEl = document.getElementById('quote-box');
+const quotesFormEl = document.getElementById('quotesForm');
+const quotesIntroEl = document.getElementById('quotesIntro');
+const quotesEndEl = document.getElementById('quotesEnd');
+const quotesExitEl = document.getElementById('exitQuotes');
+const quotesRestartEl = document.getElementById('restartQuotes');
+
+// Handle for setInterval
+let handle;
+// Handle for reload Interval
+let reloadHandle;
+// Count for number of quotes
+let count = 0;
+
+// Starts quote on Submit
+quotesFormEl.onsubmit = function(e) {
+  e.preventDefault();
+  const topicEl = document.getElementById('quotesTopic');
+  const lengthEl = document.getElementById('quotesLength');
+  const topic = topicEl.options[topicEl.selectedIndex].value;
+  const length = lengthEl.options[lengthEl.selectedIndex].value;
+
+  if (!topic) {
+    alert('Please select quotes topic!');
+    return;
+  } else if (!length) {
+    alert('Please select number of quotes!');
+    return;
+  } else {
+    quotesIntroEl.style.display = 'none';
+    generateRandomQuotesForTopic(topic, length);
+  }
+}
+
+function generateRandomQuotesForTopic(selectedTopic, selectedLength) {
+  let quotesArray = [];
+  for(let i = 0; i < quotes.length; i++) {
+    if(quotes[i].topic === selectedTopic) {
+      quotesArray.push(quotes[i]);
+    }
+  }
+  generateRandomQuote(quotesArray, selectedLength);
+}
+
+function generateRandomQuote(quotesArray, length) {
+  const shuffled = getShuffledArray(quotesArray);
+  let quote = shuffled[0];
+  printQuotesOnScreen(shuffled, quote, length);
+  // Call function to repeat quote generation
+  handle = setInterval(function() {
+    printQuotesOnScreen(shuffled, quote, length);
+  }, 5000);
+}
+
+function printQuotesOnScreen(shuffled, obj, length){
+  // Random background color
+  document.getElementById('rgb').style.background = randomColor();
+  // Make a new quote if its not supplied from above function
+  if(count != 0) {
+    obj = shuffled[count];
+  }
+  let message = '<p class="quote">' + obj.quote + '</p>'; 
+  message += '<p class="source">' + obj.source;
+  if (obj.citation !== undefined && obj.year !== undefined) {
+    message += '<span class="citation">' + obj.citation + '</span>'; 
+    message += '<span class="year">' + obj.year + '</span>' + '</p>';
+  }
+
+  message += '<h4>' + obj.tags + '</h4>';
+  quotesEl.innerHTML = message;
+  count += 1;
+
+  console.log(obj.quote);
+  if(count == length) {
+    clearInterval(handle);
+    handle = 0;
+    exitQuotes();
+  }
+}
+
+function exitQuotes() {
+  reloadHandle = setInterval(function() {
+    reloadPage();
+  }, 5000);
+}
+
+
+function reloadPage() {
+  location.reload();
+  clearInterval(reloadHandle);
+  reloadHandle = 0;
+}
+
+function randomRGB() {
+  return Math.floor(Math.random() * 256);
+}
+
+function randomColor() {
+  var color;
+  red = randomRGB();
+  green = randomRGB();
+  blue = randomRGB();
+  color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  return color;
+}
+
+
+function getShuffledArray (arr){
+  let newArr = [...arr]
+  for (let i = newArr.length - 1; i > 0; i--) {
+      const rand = Math.floor(Math.random() * (i + 1));
+      [newArr[i], newArr[rand]]=[newArr[rand], newArr[i]];
+  }
+  return newArr;
+}
